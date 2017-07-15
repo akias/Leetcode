8e44affca275ae5e28448b9555f8832fd1a92b46
akias <saikam5yday@gmail.com>
2017-07-15 15:32:30 +0900

    test pdf

--- src/Test.java
+++ src/Test.java
@@ -2,10 +2,16 @@
 
 public class Test {
 	public static void main(String args[]){
-		Test33SearchInRotatedArray btbs = new Test33SearchInRotatedArray();
-		int[] prices = {5,1,3};
-		int re = btbs.search(prices, 3);
-		System.out.println(re);
+		//Test33SearchInRotatedArray btbs = new Test33SearchInRotatedArray();
+		//int[] prices = {5,1,3};
+		//int re = btbs.search(prices, 3);
+		//System.out.println(re);
+		
+		int[][] arr = new int [2][3];//定义一个二维数组  
+		System.out.print(arr.length);
+	    System.out.println(arr[1].length);
+	    int[] brr = new int[2];
+	    System.out.println(brr.length);
 		//シングルクォーテーションとダブルクォーテーション　matchesできます
 		//String tmpString = "wode\\nidetade";  
 		//System.out.println(tmpString);
